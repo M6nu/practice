61353efe0ef10f2b4a3e3ddfac889ad0287e91bf
M6nu <emmanuel.kouohmbwang@edu.unife.it>
2024-06-06 22:50:18 +0200

    inizializazione della home

--- myApp.py
+++ myApp.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+# crea una route -> un url  si tratta di un decorator
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    stuff = "this is <strong>bold</strong> text "
+    #return "<h1>Hello, World!</h1>"
+    favorite_player=['Cr7','Guendouzi','MacTominey','B.Fernadez','Harrit','L.Modric','L.Diaz']
+    return render_template("index.html", stuff= stuff, favorite_player= favorite_player)
+
+# localhost:5000/user/Paul
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
+    # return "<h1>Hello, {}</h1>".format(name)
+    
+# Create a Custon error pages
+
+# Invalid URL 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html")
